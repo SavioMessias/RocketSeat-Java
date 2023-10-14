539367e9f90aa0d63e045424c2414d9512227f7d
Savio <kiqsavio@gmail.com>
2023-10-14 13:31:19 -0300

    adicionando funções para o usuario

---
+++ src/main/java/br/com/savio/todolist/task/TaskController.java
@@ -0,0 +1,66 @@
+package br.com.savio.todolist.task;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.savio.todolist.utils.utils;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskReposiroty taskReposiroty;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início / data de termino deve ser maior do que a data atual.");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início / data de termino deve ser maior do que a data atual.");
+        }
+
+        var task = this.taskReposiroty.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskReposiroty.findByIdUser((UUID) idUser);
+        return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+
+        var task = this.taskReposiroty.findById(id).orElse(null);
+
+        utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskReposiroty.save(task);
+    }
+}
